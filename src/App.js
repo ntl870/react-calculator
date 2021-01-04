6a55c2c8ab2dd363fc6f8b65043735b3de5817be
ntl870 <ntlong870@gmail.com>
2021-01-05 00:31:57 +0700

    finished plus

--- src/App.js
+++ src/App.js
@@ -3,58 +3,105 @@ import React, { Component } from "react";
 import Numberbtn from "./Components/Numberbtn";
 
 class App extends Component {
-  AnsTemp = [];
   state = {
     temp: [0],
-    display : 0,
     ans: 0,
+    operating: true,
+    plus: false,
+    subtract: false,
+    multiply: false,
+    divide: false,
   };
+
+  AnsTemp = [];
 
   getInput = (e) => {
     let ButtonValue = e.target.value;
-    this.AnsTemp.push(ButtonValue);
     let temp = this.AnsTemp;
-    temp = temp.join("");
+    temp.push(ButtonValue);
+    temp = temp.join('');
     temp = Number(temp);
     this.setState({
       ...this.state,
-      temp: temp,
-    });
+      operating : true,
+      temp : temp
+    })
   };
+
 
   operatorPlus = () => {
     let tempState = this.state;
+    tempState.ans = tempState.ans + tempState.temp;
     this.AnsTemp = [];
-    tempState.temp = Number(tempState.temp);
-    tempState.ans += tempState.temp;
-    tempState.temp = [0];
+    tempState.temp = [];
+    tempState.operating = false;
+    tempState.plus = true;
     this.setState({
-      ...this.state,
-      temp : tempState.temp,
-      ans: tempState.ans
-    })
-  };
-
-  operatorEqual = () => {
-    let tempState = this.state;
-    this.setState({
-      ...this.state,
-      display : tempState.ans
+      tempState
     })
   }
 
+  operatorSubtract = () => {
+    let tempState = this.state;
+    tempState.ans = tempState.ans - tempState.temp;
+    this.AnsTemp = [];
+    tempState.temp = [];
+    tempState.operating = false;
+    tempState.subtract = true;
+    this.setState({
+      tempState
+    })
+  }
+
+  operatorEqual = () => {
+    let tempState = this.state;
+    if (this.state.plus) {
+      tempState.ans += tempState.temp;
+      tempState.temp = 0;
+      tempState.plus = false;
+      tempState.operating = false;
+      this.setState({
+        tempState
+      });
+    }
+    if (this.state.subtract) {
+      tempState.ans -= tempState.temp;
+      tempState.temp = 0;
+      tempState.subtract = false;
+      tempState.operating = false;
+      this.setState({
+        tempState
+      });
+    }
+
+  };
+
   render() {
     console.log(this.state);
-    return (
-      <div className="App">
-        <Numberbtn
-          getInput={this.getInput}
-          current={this.state.display}
-          equal={this.operatorEqual}
-          plus={this.operatorPlus}
-        />
-      </div>
-    );
+    if (this.state.operating) {
+      return (
+        <div className="App">
+          <Numberbtn
+            getInput={this.getInput}
+            display={this.state.temp}
+            equal={this.operatorEqual}
+            plus={this.operatorPlus}
+          />
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <Numberbtn
+            getInput={this.getInput}
+            display={this.state.ans}
+            equal={this.operatorEqual}
+            plus={this.operatorPlus}
+            subtract={this.operatorSubtract}
+          />
+        </div>
+      );
+    }
   }
 }
 
--- src/Components/Numberbtn.js
+++ src/Components/Numberbtn.js
@@ -7,58 +7,134 @@ const Numberbtn = (props) => {
         <input
           type="text"
           className="text-light calculator-screen z-depth-1"
-          value={props.current}
+          value={props.display}
           disabled
         />
 
         <div className="calculator-keys">
-          <button id="math-sign" type="button" className="operator btn" value="+" onClick={props.plus}>
+          <button
+            id="math-sign"
+            type="button"
+            className="operator btn"
+            value="+"
+            onClick={props.plus}
+          >
             +
           </button>
-          <button id="math-sign" type="button" className="operator btn" value="-">
+          <button
+            id="math-sign"
+            type="button"
+            className="operator btn"
+            value="-"
+            onClick={props.subtract}
+          >
             -
           </button>
-          <button id="math-sign" type="button" className="operator btn" value="*">
+          <button
+            id="math-sign"
+            type="button"
+            className="operator btn"
+            value="*"
+          >
             &times;
           </button>
-          <button id="math-sign" type="button" className="operator btn" value="/">
+          <button
+            id="math-sign"
+            type="button"
+            className="operator btn"
+            value="/"
+          >
             &divide;
           </button>
 
-          <button type="button" value="7" className="btn btn-dark waves-effect" onClick={props.getInput}>
+          <button
+            type="button"
+            value="7"
+            className="btn btn-dark waves-effect"
+            onClick={props.getInput}
+          >
             7
           </button>
-          <button type="button" value="8" className="btn btn-dark waves-effect" onClick={props.getInput}>
+          <button
+            type="button"
+            value="8"
+            className="btn btn-dark waves-effect"
+            onClick={props.getInput}
+          >
             8
           </button>
-          <button type="button" value="9" className="btn btn-dark waves-effect" onClick={props.getInput}>
+          <button
+            type="button"
+            value="9"
+            className="btn btn-dark waves-effect"
+            onClick={props.getInput}
+          >
             9
           </button>
 
-          <button type="button" value="4" className="btn btn-dark waves-effect" onClick={props.getInput}>
+          <button
+            type="button"
+            value="4"
+            className="btn btn-dark waves-effect"
+            onClick={props.getInput}
+          >
             4
           </button>
-          <button type="button" value="5" className="btn btn-dark waves-effect" onClick={props.getInput}>
+          <button
+            type="button"
+            value="5"
+            className="btn btn-dark waves-effect"
+            onClick={props.getInput}
+          >
             5
           </button>
-          <button type="button" value="6" className="btn btn-dark waves-effect" onClick={props.getInput}>
+          <button
+            type="button"
+            value="6"
+            className="btn btn-dark waves-effect"
+            onClick={props.getInput}
+          >
             6
           </button>
 
-          <button type="button" value="1" className="btn btn-dark waves-effect" onClick={props.getInput}>
+          <button
+            type="button"
+            value="1"
+            className="btn btn-dark waves-effect"
+            onClick={props.getInput}
+          >
             1
           </button>
-          <button type="button" value="2" className="btn btn-dark waves-effect" onClick={props.getInput}>
+          <button
+            type="button"
+            value="2"
+            className="btn btn-dark waves-effect"
+            onClick={props.getInput}
+          >
             2
           </button>
-          <button type="button" value="3" className="btn btn-dark waves-effect" onClick={props.getInput}>
+          <button
+            type="button"
+            value="3"
+            className="btn btn-dark waves-effect"
+            onClick={props.getInput}
+          >
             3
           </button>
 
-          <button type="button" value="0" className="btn btn-dark waves-effect" onClick={props.getInput}>
+          <button
+            type="button"
+            value="0"
+            className="btn btn-dark waves-effect"
+            onClick={props.getInput}
+          >
             0
           </button>
-          <button type="button" className="decimal function btn btn-dark" value=".">
+          <button
+            type="button"
+            className="decimal function btn btn-dark"
+            value="."
+          >
             .
           </button>
           <button
@@ -72,7 +148,7 @@ const Numberbtn = (props) => {
           <button
             type="button"
             className="equal-sign operator btn btn-primary"
-            value="=" 
+            value="="
             onClick={props.equal}
           >
             =
